3ac66b3e911677aa53056a5397677b6590740d22
Miningdog95 <dmitrybelyaev2015@gmail.com>
2022-11-22 20:22:30 +0300

    Version_1

--- src/Test/Reverse.java
+++ src/Test/Reverse.java
@@ -10,16 +10,20 @@ public class Reverse {
         int i;
         int j;
         String Vvod = scanner.nextLine();
+        String arrayTest[] = new String[160];
         String array[] = Vvod.split(" ");
+        if (array.length < arrayTest.length) {
+            for (i = 0; i < array.length; i++) {
+                System.out.println(array[i]);
+            }
+            System.out.println("Reverse");
 
-        for (i = 0; i < array.length; i++) {
-            System.out.println(array[i]);
-        }
-        System.out.println("Reverse");
-
-        for (j = array.length - 1; j >= 0; j--) {
-            int result = Integer.parseInt(array[j]);
-            System.out.println(result);
+            for (j = array.length - 1; j >= 0; j--) {
+                int result = Integer.parseInt(array[j]);
+                System.out.println(result);
+            }
+        } else {
+            System.out.println("Число больше 160");
         }
     }
 }
